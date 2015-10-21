9c83e95a294d996c80ffc6b82bc0c2f605051cc0
Matt Pascoe <matt@opennetadmin.com>
2015-10-21 10:52:04 -0700

    check for local config file in /etc/build_tinydns.conf. fixes issue #2

--- plugin_info.php
+++ plugin_info.php
@@ -4,7 +4,7 @@ Your plugin must have a short description and a version number
 */
 
 $plugin_description = 'Build TinyDNS server configurations.';
-$plugin_version = '1.1';
+$plugin_version = '1.2';
 $plugin_help_url = 'http://opennetadmin.com/docs/build_tinydns';
 
 
