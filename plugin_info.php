e6d1cdd08c7b4679e46e1d5552727f9362c55603
Matt Pascoe <matt@opennetadmin.com>
2015-10-26 13:22:07 -0700

    fixing plugin version info

--- plugin_info.php
+++ plugin_info.php
@@ -2,10 +2,11 @@
 /*
 Your plugin must have a short description and a version number
 */
+$plug_name = basename(dirname(__FILE__));
 
 $plugin_description = 'Build TinyDNS server configurations.';
 $plugin_version = '1.2';
-$plugin_help_url = 'http://opennetadmin.com/docs/build_tinydns';
+$plugin_help_url = 'https://github.com/opennetadmin/'.$plug_name.'/wiki';
 
 
 ?>
